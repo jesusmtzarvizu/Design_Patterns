f75bfa609aee3ab769ff8571ae006ce10c223ba5
jesusmtzarvizu <jarvizu@nearsoft.com>
2017-01-11 21:29:30 -0700

    Create Animal.java

---
+++ FactoryMethod/Animal.java
@@ -0,0 +1,12 @@
+
+// Step 1 : Create a Simple interface for polymorphic
+// behavior ... here it is Animal having two simple 
+// method as eat() and sleep()
+ 
+public interface Animal {
+ 
+ public void eat();
+ 
+ public void sleep();
+ 
+}
