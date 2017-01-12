912ac12960375a324a798fe7fcb7185dba594c6f
jesusmtzarvizu <jarvizu@nearsoft.com>
2017-01-11 21:51:25 -0700

    Update AnimalFactory.java

--- FactoryMethod/AnimalFactory.java
+++ FactoryMethod/AnimalFactory.java
@@ -1,20 +1,19 @@
 //Create a class which will act as a factory for 
 // creating concrete objects of Animal 
 
-public class AnimalFactory{
+public class AnimalFactory {
 
-	public Animal createAnimal(String animalType){
-        	if(animalType==null){
-	        	   return null;	
-		      }
-		      if("dog".equalsIgnoreCase(animalType)){
-			        return Dog();		
-		      }
-		      else if("cat".equalsIgnoreCase(animalType)){
-			        return Cat();		
-		      }
-          
-		      return null;
-	
-	  }
+    public Animal createAnimal(String animalType) {
+        if (animalType == null) {
+            return null;
+        }
+        if ("dog".equalsIgnoreCase(animalType)) {
+            return Dog();
+        } else if ("cat".equalsIgnoreCase(animalType)) {
+            return Cat();
+        }
+
+        return null;
+
+    }
 }
