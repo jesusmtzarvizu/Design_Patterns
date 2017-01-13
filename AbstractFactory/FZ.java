0fea8c1f8701eb7d0361d9b99d5420c1bc448fdc
jesusmtzarvizu <jarvizu@nearsoft.com>
2017-01-12 20:15:09 -0700

    Create FZ.java

---
+++ AbstractFactory/FZ.java
@@ -0,0 +1,12 @@
+//Concrete Product (FZ) for Abstract Product (Bike).
+
+public class FZ implements Bike {
+
+    @Override
+    public void drive() {
+
+        System.out.println("FZ is driving ... ");
+
+    }
+
+}
