e2ed7124a564cceccc6a0212b91809b69759e771
jesusmtzarvizu <jarvizu@nearsoft.com>
2017-01-12 20:14:14 -0700

    Create Hunk.java

---
+++ AbstractFactory/Hunk.java
@@ -0,0 +1,13 @@
+ //Concrete Product (Hunk) for Abstract Product (Bike).
+ 
+ public class Hunk implements Bike {
+
+    @Override
+    public void drive() {
+
+        System.out.println("Hunk is driving ...");
+
+    }
+
+}
+ 
